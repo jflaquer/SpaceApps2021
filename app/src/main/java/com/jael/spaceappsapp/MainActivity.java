85d11b37453a3525e17655191fba26763b73c916
Jael Flaquer <jflaquer@udel.edu>
2021-10-03 17:04:47 -0400

    Created a basic Android app layout for the sunshine tracker app described in the 'You Are My Sunshine' Space Apps Challenge

---
+++ app/src/main/java/com/jael/spaceappsapp/MainActivity.java
@@ -0,0 +1,77 @@
+package com.jael.spaceappsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_getCityID, btn_getWeatherByCityID, btn_getWeatherByName;
+    EditText et_dataInput;
+    ListView lv_weatherReports;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // assigns values to each control on the layout
+        btn_getCityID = findViewById(R.id.btn_getCityID);
+        btn_getWeatherByCityID = findViewById(R.id.btn_getWeatherByCityID);
+        btn_getWeatherByName = findViewById(R.id.btn_getWeatherByName);
+
+        et_dataInput = findViewById(R.id.et_dataInput);
+        lv_weatherReports = findViewById(R.id.lv_weatherReports);
+
+        //click listeners for each button
+        btn_getWeatherByCityID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url ="https://www.google.com";
+
+// Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                textView.setText("Response is: "+ response.substring(0,500));
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("That didn't work!");
+                    }
+                });
+
+// Add the request to the RequestQueue.
+                queue.add(stringRequest);
+                Toast.makeText(MainActivity.this, "Clicked 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_getCityID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Clicked 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You typed " + et_dataInput.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
